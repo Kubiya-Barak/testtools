98bca45b8af5bc16e7d168b99c4d94ef405599ac
Kubiya-Barak <barak.nagar@kubiya.ai>
2024-10-06 16:19:04 +0300

    fix

--- tools/pager_duty/page_oncall_engineer/page_oncall_engineer.py
+++ tools/pager_duty/page_oncall_engineer/page_oncall_engineer.py
@@ -2,9 +2,8 @@ from kubiya_sdk.tools import Arg
 from .base import PagerDuty
 from kubiya_sdk.tools.registry import tool_registry
 
-# Slack Send Message Tool
-pager_duty = PagerDuty(
-   long_running = false
-)
+# PagerDuty Tool
+pager_duty = PagerDuty(long_running=False)
 
+# Register the PagerDuty tool
 tool_registry.register("pager_duty", pager_duty)
